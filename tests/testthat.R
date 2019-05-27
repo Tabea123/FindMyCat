511348c6553e815888100b60f890017289ffd6b5
Tabea123 <tabea.hoffmann@metax.org>
2019-05-27 11:02:08 +0200

    test lookup_situation()

--- tests/testthat.R
+++ tests/testthat.R
@@ -52,3 +52,13 @@ test_that("create_population creates a list containing data.frames", {
   expect_is(model[[1]], "data.frame")
 })
 
+# function lookup_situation
+test_that("stop messages occur correctly", {
+  grid <- create_grid(c(5, 5), 5)
+  expect_error(lookup_situation(individual, grid, latitude = 2, longitude = 3))
+  individual <- create_population(10)[[10]]
+  expect_error(lookup_situation(individual, grid, latitude = 7, longitude = 3))
+  expect_error(lookup_situation(individual, grid, latitude = c(7, 8, 7, 9, 7), longitude = 3))
+})
+
+
