465cc1dbdced03060cdd965f765e0addc3759d04
Tabea123 <tabea.hoffmann@metax.org>
2019-05-28 11:32:04 +0200

    testing of life_cycle()

--- tests/testthat.R
+++ tests/testthat.R
@@ -87,10 +87,10 @@ test_that("stop messages occur correctly", {
   expect_error(move_score(individual, grid, latitude = 2, longitude = 3, steps = 10))
 })
 
-test_that("score cannot be over max score", {
+test_that("score cannot be over the maximum possibe score of n_evidence * 10", {
   grid <- create_grid(c(5, 5), 5)
   individual <- create_population(10)[[10]]
-  expect_less_than(move_score(individual, grid, steps = 200)$score, 50)
+  expect_less_than(move_score(individual, grid, steps = 200)$score, 51)
 })
 
 test_that("move_score returns a numeric score", {
@@ -116,15 +116,25 @@ test_that("score cannot be over max score", {
 # function life
 test_that("stop messages occur correctly", {
   population <- create_population(10)
-  expect_error(life(population, grid_size = "a", n_evidence = 10, steps = 3,
+  expect_error(life_cycle(population, grid_size = "a", n_evidence = 10, steps = 3,
                     sessions = 2))
-  expect_error(life(population, grid_size = c(10, 5), n_evidence = 10, steps = 3,
+  expect_error(life_cycle(population, grid_size = c(10, 5), n_evidence = 10, steps = 3,
                     sessions = "three"))
+  expect_error(life_cycle(population = population, grid_size = c(10, 10), steps = 100,
+                          sessions = 1))
 })
 
 test_that("life returns a matrix of numbers", {
   population <- create_population(10)
-  model <- life(population, grid_size = c(5, 5), n_evidence = 11, steps = 3, sessions = 3)
+  model <- life_cycle(population, grid_size = c(5, 5), n_evidence = 11, steps = 3,
+                sessions = 3)
   expect_is(model, "matrix")
   expect_is(model[1,1], "numeric")
 })
+
+test_that("score cannot be over the maximum possibe score of n_evidence * 10", {
+  population <- create_population(10)
+  expect_lt(sum(life_cycle(population, grid_size = c(5, 5), n_evidence = 11,
+                            steps = 3, sessions = 3) > 5*11), 1)
+})
+
