bd07e0ba8966439f654d659f578eca11a0d898da
Tabea123 <tabea.hoffmann@metax.org>
2019-05-30 18:43:42 +0200

    saving the different trials as png

--- R/Plotting Scores.R
+++ R/Plotting Scores.R
@@ -19,10 +19,13 @@ evolution <- function(population_size, grid_size, n_evidence, steps, sessions, g
 }
 
 scores <- evolution(population_size = 100, grid_size = c(5, 5),
-                    n_evidence = 10, steps = 50, sessions = 20, generations = 30)
+                    n_evidence = 10, steps = 50, sessions = 10, generations = 70)
 
-x <- numeric(10)
-for(t in 1:10){
+x <- numeric(50)
+for(t in 1:50){
   x[t] <- mean(scores[[t]])
 }
+
+png("70g20se50st100i.png")
 plot(x)
+dev.off()
